6a35eb4901e40e4e007d71e3bb1771bf71d43db9
Hello2Hdllo <learningwebdevelopment852@gmail.com>
2025-07-09 13:54:57 +0800

    update

--- updatelog.js
+++ updatelog.js
@@ -1,4 +1,16 @@
 console.log("Hello, JavaScript!")
-const min = 0
+const min = 1
 const max = 10
 console.log(Math.floor(Math.random() * max) + min)
+const a = document.getElementById("div")
+a.onclick = function(){
+  alert("July, 8 2025")
+}
+const b = document.getElementById("div2")
+b.onclick = function(){
+  alert("July 8, 2025")
+}
+const c = document.getElementById("div3")
+c.onclick = function(){
+  alert("July 9, 2025")
+}
